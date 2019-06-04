3fda6928b53afec73ecb8dd426e2d90b9f9d8f8f
Israel Cavalcante de Lucena <icl@CESAR-LDAP.INTRANET>
2019-06-04 09:01:07 -0300

    initial studies

---
+++ desafio.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#desafio', 
+    data:{
+        nome: 'Ana',
+        idade: 28,
+        imagem: 'http://files.cod3r.com.br/curso-vue/vue.jpg',
+    },
+    methods:{
+    }
+})
